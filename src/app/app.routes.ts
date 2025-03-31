50587160753cfa5ffe5a031923cb9fbc6dfe18f6
Tomáš Roj <rojtomas@email.cz>
2025-03-31 15:27:45 +0200

    Added basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistraceComponent } from './pages/registrace/registrace.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'Login', component: LoginComponent},
+    { path: 'Register', component: RegistraceComponent}
+];
