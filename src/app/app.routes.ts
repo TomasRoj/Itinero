2d9823da43b5add3bb194abc27b9d3a1bbc4933f
Hodanko <158205046+Hodanko@users.noreply.github.com>
2025-04-01 19:10:26 +0200

    added setting page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistraceComponent } from './pages/registrace/registrace.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'Login', component: LoginComponent},
     { path: 'Register', component: RegistraceComponent},
+    { path: 'Settings', component: SettingsComponent},
 ];
+    
