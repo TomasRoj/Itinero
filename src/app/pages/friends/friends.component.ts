2fb38bbaa1aadb241c351cb5b77b4dff7c4a9289
Hodanko <158205046+Hodanko@users.noreply.github.com>
2025-04-14 09:57:48 +0200

    Major changes to the friends class

--- src/app/pages/friends/friends.component.ts
+++ src/app/pages/friends/friends.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { RouterLink } from '@angular/router';
+
+
+interface Friend {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-friends',
@@ -9,22 +16,56 @@ import { RouterLink } from '@angular/router';
   templateUrl: './friends.component.html',
   styleUrl: './friends.component.scss'
 })
-export class FriendsComponent {
-  members = [
-    { 
-      name: 'Kája Horáková', 
-      role: 'Vlastník', 
-      image: 'assets/images/profile1.jpg' 
-    },
-    { 
-      name: 'Honza Novák', 
-      role: 'Member', 
-      image: 'assets/images/profile2.jpg' 
-    },
-    { 
-      name: 'Tomáš Veselý', 
-      role: 'Member', 
-      image: 'assets/images/profile3.jpg' 
-    }
-  ]
+
+export class FriendsComponent implements OnInit {
+  friends: Friend[] = [];
+  newFriendId: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialize with example friends data (similar to the screenshot)
+    this.friends = [
+      { 
+        id: 'D56LAX', 
+        name: 'Honza Novák', 
+        avatar: 'assets/avatars/avatar1.png' 
+      },
+      { 
+        id: 'R18JFK', 
+        name: 'Tomáš Veselý', 
+        avatar: 'assets/avatars/avatar2.png' 
+      },
+      { 
+        id: 'U44ATL', 
+        name: 'Petr Šťastný', 
+        avatar: 'assets/avatars/avatar3.png' 
+      },
+      { 
+        id: 'S26PRG', 
+        name: 'Ms. Jacksonová', 
+        avatar: 'assets/avatars/avatar4.png' 
+      }
+    ];
+  }
+  
+  addFriend(): void {
+    if (!this.newFriendId) return;
+    
+    // In a real app, you would make an API call to validate the ID and get user info
+    // For this example, we'll add a placeholder friend
+    const newFriend: Friend = {
+      id: this.newFriendId,
+      name: 'Nový Přítel', // This would come from your API in a real app
+      avatar: 'assets/avatars/default.png'
+    };
+
+    this.friends.push(newFriend);
+    this.newFriendId = ''; // Reset the input
+  }
+
+  removeFriend(id: string): void {
+    // Remove the friend with the matching ID
+    this.friends = this.friends.filter(friend => friend.id !== id);
+  }
 }
